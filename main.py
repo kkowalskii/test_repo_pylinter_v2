bd1c28fb83bf4e1072c2011086d8d2551346fb00
Kosma Kowalski <k.kowalski.655@studms.ug.edu.pl>
2022-01-25 22:07:28 +0100

    zobaczymy czy dziala

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-import math
-import os
-
-
-def add(a, b) -> int:
-    return math.floor(a + b)
-
-
-def to_sentence(s) -> str:
-    s = s.capitalize()
-
-    if s.endswith('.'):
-        return s
-    else:
-        return s + '.'
+def add(a, b):
+    return (a + b)
--- test/test_main.py
+++ test/test_main.py
@@ -2,13 +2,8 @@ import main
 
 
 def test_add():
-    assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
-
-
-def test_to_sentence():
-    assert main.to_sentence('apple') == 'Apple.'
-    assert main.to_sentence('Apple trees') == 'Apple trees.'
-    assert main.to_sentence('Apple trees.') == 'Apple trees.'
+    assert main.add(1, 2) == 3
+    assert main.add(1,8) == 9
+    assert main.add(1,8) == 12
+    assert main.add(1,4) == 5
+    
